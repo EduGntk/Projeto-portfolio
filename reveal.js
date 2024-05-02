4fb6bb1033c77112181a56c06b9fa78c05a22ba8
Eduardo Gnatkowski <Eduardo.gnat@gmail.com>
2024-05-01 22:45:36 -0300

    Adicionando efeitos com JavaScript

---
+++ reveal.js
@@ -0,0 +1,72 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.reveal_topo',{
+    duration: 2000,
+    distance: '90px',
+    delay: 200,
+})
+
+revelar.reveal('.reveal_topo1',{
+    duration: 2000,
+    distance: '90px',
+    delay: 400,
+})
+
+revelar.reveal('.reveal_topo2',{
+    duration: 2000,
+    distance: '90px',
+    delay: 600,
+})
+
+revelar.reveal('.reveal_topo3',{
+    duration: 2000,
+    distance: '90px',
+    delay: 800,
+})
+
+revelar.reveal('.reveal_topo4',{
+    duration: 2000,
+    distance: '190px',
+    delay: 800,
+})
+
+revelar.reveal('.reveal_card1',{
+    duration: 500,
+    distance: '90px',
+    delay: 700,
+    origin: 'left'
+})
+
+revelar.reveal('.reveal_card2',{
+    duration: 500,
+    distance: '90px',
+    delay: 500,
+    origin: 'left'
+})
+
+revelar.reveal('.reveal_card3',{
+    duration: 500,
+    distance: '90px',
+    delay: 200,
+    origin: 'left'
+})
+
+revelar.reveal('.reveal_sobre',{
+    duration: 2000,
+    distance: '90px',
+    delay: 200,
+    origin: 'left'
+})
+
+revelar.reveal('.reveal_sobre2',{
+    duration: 3000,
+    delay: 200,
+    opacity: 0
+})
+
+revelar.reveal('.reveal_img_sobre',{
+    duration: 2000,
+    delay: 200,
+    distance: '90px',
+    origin: 'right'
+})
